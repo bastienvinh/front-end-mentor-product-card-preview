4c5959c02071beefcd362074ef087c82b0d3a579
Bastien VINH <bvinh.dev@yahoo.com>
2023-01-19 20:55:37 +0100

    Add alt on images

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,14 +10,14 @@ import './ProductCard.scss'
 const ProductCard = () => {
   return (
     <div id="block-product">
-      <img className='image-desktop' src={ProductImage}></img>
-      <img className='image-mobile' src={ProductImageMobile}></img>
+      <img alt='image-desktop' className='image-desktop' src={ProductImage}></img>
+      <img alt='image-mobile' className='image-mobile' src={ProductImageMobile}></img>
       <div id='block-product-details'>
         <h4>Perfume</h4>
         <h1>Gabrielle Essence Eau De Parfum</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
         <Price oldPrice={169.99} currentPrice={149.99} />
-        <Button><img src={IconCart} /> Add To Cart</Button>
+        <Button><img alt='icon-cart' src={IconCart} /> Add To Cart</Button>
       </div>
     </div>
   )
